10429e246dfe2a50adb4a14d0c96830685b5e8d9
Roberto Castro <recastro@presidencia.gob.sv>
2022-06-03 09:06:22 -0600

    Configuracion de whitenoise y procfiel 	modificados:     .gitignore 	nuevos archivos: Procfile 	modificados:     pocket/settings.py 	modificados:     requirements.txt 	nuevos archivos: static/.gitkeep

--- pocket/settings.py
+++ pocket/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,8 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
